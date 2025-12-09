9139f383eb5c95d91633b64ed6cda4d935face88
qxiang8 <qxiang8@wisc.edu>
2025-12-09 10:36:18 -0600

    finalVerison2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/cs571-project/' : '/',
-  build: {
-    outDir: 'docs'
-  }
+plugins: [react()],
+base: '/p32/',
+build: {
+outDir: 'docs'
+}
 })
